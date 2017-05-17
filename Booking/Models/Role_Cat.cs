9f502f346c74dbffe836c65f86d4408ea8014bd9
luckystar <luckystar.cntt@gmail.com>
2017-05-17 10:33:23 +0700

    Update news

---
+++ Booking/Models/Role_Cat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Booking.Models
+{
+    public class Role_Cat
+    {
+        public Role_Cat()
+        {
+            this.ROLEs = new HashSet<ROLE>();
+            this.CATEGORies = new HashSet<CATEGORY>();
+        }
+        public decimal ROLE_ID { get; set; }
+        public decimal CATEGORY_ID { get; set; }
+        public virtual ICollection<ROLE> ROLEs { get; set; }
+        public virtual ICollection<CATEGORY> CATEGORies { get; set; }
+    }
+}
